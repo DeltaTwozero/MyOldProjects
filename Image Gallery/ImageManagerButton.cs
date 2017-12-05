7a41ca5371e8e077bc1008ec335d783e49d3e794
DeltaTwoZero <deltatwozerosam@gmail.com>
2017-12-05 13:35:05 -0500

    Image gallery
    The very first homework I've done in unity and in my life.

---
+++ Image Gallery/ImageManagerButton.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ImageManagerButton : MonoBehaviour
+{
+    [SerializeField]
+    Button _nextImg;
+
+    [SerializeField]
+    Button _prevImg;
+
+    [SerializeField]
+    Sprite[] _imgs;
+
+    [SerializeField]
+    Image _show;
+
+    [SerializeField]
+    int _currentImg;
+
+    void Start()
+    {
+        _currentImg = 0;
+        _show.sprite = _imgs[_currentImg];
+    }
+
+    public void NextImg()
+    {
+        if (_currentImg + 1 < _imgs.Length)
+        {
+            print("++ works");
+            _currentImg++;
+        }
+        else
+        {
+            _currentImg = 0;
+        }
+        _show.sprite = _imgs[_currentImg];
+    }
+
+    public void PrevImg()
+    {
+        if (_currentImg - 1 > -1)
+        {
+            print("-- works");
+            _currentImg--;
+        }
+        else
+        {
+            _currentImg = _imgs.Length - 1;
+        }
+        _show.sprite = _imgs[_currentImg];
+    }
+}
